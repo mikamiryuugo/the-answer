f99138a5f54d43c828f14c9d4f7c41d97db386b9
mikamiryu <mikamiryuugo@yahoo.co.jp>
2020-07-07 14:12:23 +0900

    [add]投稿フォームにバリデーション追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,26 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:notice] = "投稿しました"
+      redirect_to root_url
+    else
+      render 'new'
+    end
+
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:question_title, :answer)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :question_title, presence: true, length: { maximum: 100 }
+  validates :answer, presence: true, length: { maximum: 300 }
 end
