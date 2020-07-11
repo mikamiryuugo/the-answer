d8aaacc715cf5f7922bbf587e65343321dc548a8
mikamiryu <mikamiryuugo@yahoo.co.jp>
2020-07-11 18:25:09 +0900

    [add]ヘッダーに「お気に入り」「自分の投稿」へのリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   get 'users/show'
-  get 'post_likes/create'
-  get 'post_likes/destroy'
   root 'tops#index'
   resources :posts
-  resources :post_likes, only: [:create, :destroy]
+  resources :post_likes, only: [:create, :destroy, :index]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
